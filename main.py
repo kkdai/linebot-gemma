ab801fc097a59963213fab577f405c386456107a
Evan Lin <evan.if.lin@linecorp.com>
2024-07-25 01:36:21 +0800

    Refactor handle_callback function to include safe message and reply in zh-TW

--- main.py
+++ main.py
@@ -112,7 +112,8 @@ async def handle_callback(request: Request):
                 safe_ret = generate_local_llm_result(f'{remove_personal_prompt}, {msg}')
                 # pass the result to gemini to generate a complete sentence.
                 ret = generate_gemini_text_complete(f'{safe_ret}, reply in zh-TW:')
-                reply_msg = TextSendMessage(text=ret.text)
+                reply_msg = TextSendMessage(text=f'原有訊息: {msg}, 修正後安全訊息： {ret.text}')
+                reply_msg = TextSendMessage(text=f'回覆訊息： {ret.text}')
                 await line_bot_api.reply_message(
                     event.reply_token,
                     reply_msg
